c63753ffd1a12e97ac09cc92257719add407bdf9
Adilson Cambamba <68228579+adilson2420@users.noreply.github.com>
2020-11-19 09:29:46 +0100

    Esquelecto

--- visualcliente/src/app/app.component.ts
+++ visualcliente/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'visualcliente';
+  users: any;
+
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser() { 
+    this.http.get('https://localhost:5001/api/user').subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    })
+  }
 }
